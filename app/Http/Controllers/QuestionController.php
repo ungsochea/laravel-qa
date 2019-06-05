90c13a1012212cb0a07691af3f7fc9d61437da84
ungsochea <sochea.ung@gmail.com>
2019-06-05 10:51:55 +0700

    Add auth infor and question creation date in question item

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -14,8 +14,8 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        $questions = Question::latest()->paginate(5);
-        return view('questions.index',compact('questions'));
+        $questions = Question::with('user')->latest()->paginate(10);
+        return view('questions.index',compact('questions'))->render();
     }
 
     /**
--- app/Question.php
+++ app/Question.php
@@ -9,12 +9,20 @@ class Question extends Model
     protected $fillable = ['title','body'];
 
     public function user(){
-        return $this->belongTo(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function setTitleAttribute($vale){
         $this->attributes['title'] = $vale;
         $this->attributes['slug'] = str_slug($vale);
+    }
+
+    public function getUrlAttribute(){
+        return route("questions.show",$this->id);
+    }
+
+    public function getCreatedDateAtAttribute(){
+        return $this->created_at->diffForHumans();
     }
 
 }
