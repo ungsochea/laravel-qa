cc4c53acfb61bb638d4234d860b46e0d3d4028a6
ungsochea <sochea.ung@gmail.com>
2019-06-05 11:50:07 +0700

    Building Question Form

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -25,7 +25,9 @@ class QuestionController extends Controller
      */
     public function create()
     {
-        //
+        $question = new Question();
+
+        return view('questions.create',compact('question'));
     }
 
     /**
