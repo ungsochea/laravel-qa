eec7806929499d0b4dad723bd85072e7d048e100
ungsochea <sochea.ung@gmail.com>
2019-06-05 16:48:01 +0700

    Delete question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -87,6 +87,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return redirect('/questions')->with('success','Your question has been deleted');
     }
 }
