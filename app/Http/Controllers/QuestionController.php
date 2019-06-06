00dc211962a6ffb7cc71127db93a7484be90c036
ungsochea <sochea.ung@gmail.com>
2019-06-06 09:45:58 +0700

    Implement authorizaition using Policy

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -8,6 +8,9 @@ use Illuminate\Http\Request;
 
 class QuestionController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth',['except'=>['index','show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -65,6 +68,7 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {
+        $this->authorize("update",$question);
         return view('questions.edit',compact('question'));
     }
 
@@ -77,6 +81,7 @@ class QuestionController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
+        $this->authorize("update",$question);
         $question->update($request->only('title','body'));
         return redirect('/questions')->with('success','Your question has been updated');
     }
@@ -89,6 +94,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
+        $this->authorize("delete",$question);
         $question->delete();
         return redirect('/questions')->with('success','Your question has been deleted');
     }
